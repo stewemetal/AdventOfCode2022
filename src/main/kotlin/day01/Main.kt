0491e1f1ad472cc2ab0fce72e119c022e84dbf61
István Juhos <stewemetal@gmail.com>
2022-12-05 03:28:05 +0100

    Add solution for Day 01 1st puzzle

--- src/main/kotlin/day01/Main.kt
+++ src/main/kotlin/day01/Main.kt
@@ -1,7 +1,21 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println(
+        object {}.javaClass.getResourceAsStream("day01_input.txt")
+            .bufferedReader()
+            .readLines()
+            .run {
+                var maxValue = 0L
+                var remainingList = this
+                while (remainingList.isNotEmpty()) {
+                    val currentFoodValue = remainingList
+                        .takeWhile { it.isNotBlank() }
+                        .sumOf { it.toLong() }
+                    if (currentFoodValue > maxValue) {
+                        maxValue = currentFoodValue
+                    }
+                    remainingList = remainingList.dropWhile { it.isNotBlank() }.drop(1)
+                }
+                maxValue
+            }
+    )
 }
