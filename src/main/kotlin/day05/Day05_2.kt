f53d68b59a6d0fa407a79483d5822ab0965e6c1b
István Juhos <stewemetal@gmail.com>
2022-12-06 00:16:12 +0100

    Add solution for Day 05 2nd puzzle

---
+++ src/main/kotlin/day05/Day05_2.kt
@@ -0,0 +1,53 @@
+package day05
+
+import readFile
+import java.util.*
+
+fun main() {
+    val lines = readFile("day05_input.txt")
+    val indexOfBlankLine = lines.indexOfFirst { it.isBlank() }
+    val stacksData = lines.subList(0, indexOfBlankLine - 1).reversed()
+    val movesData = lines.subList(indexOfBlankLine + 1, lines.size)
+
+    val stackNumberLine = lines.subList(indexOfBlankLine - 1, indexOfBlankLine)[0]
+    val numberOfStacks = stackNumberLine.trim().split("   ").size
+
+    val stackCharacterIndices = (1..numberOfStacks).map { stackNumber ->
+        stackNumberLine.indexOfFirst { it.toString() == stackNumber.toString() }
+    }
+
+    val stacks = buildMap {
+        (1..numberOfStacks).forEach {
+            put(it, Stack<Char>())
+        }
+    }
+
+    stackCharacterIndices.forEachIndexed { stackIndex, stackCharacterIndex ->
+        stacksData.forEach { stackLine ->
+            val stackCharacter = stackLine.getOrNull(stackCharacterIndex)
+            if (stackCharacter != null && stackCharacter != ' ') {
+                stacks.getValue(stackIndex + 1).push(stackCharacter)
+            }
+        }
+    }
+
+    movesData.forEach { moveData ->
+        val (_, numberOfCrates, fromStack, toStack) =
+            Regex("move (\\d*) from (\\d*) to (\\d*)")
+                .find(moveData)!!
+                .groupValues
+
+        val crateCache = buildList {
+            repeat(numberOfCrates.toInt()) {
+                add(stacks.getValue(fromStack.toInt()).pop())
+            }
+        }
+        crateCache.reversed().forEach {
+            stacks.getValue(toStack.toInt()).push(it)
+        }
+    }
+
+    println(
+        stacks.map { it.value.peek() }.joinToString("")
+    )
+}
